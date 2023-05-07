29cd031f6dce2c2e837808f22413771fdbbf65e6
Bui Viet Anh <buivietanh399@gmail.com>
2023-05-07 16:24:00 +0700

    add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
-
+import { Suspense, lazy } from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from '../layouts/dashboard';
 import LogoOnlyLayout from '../layouts/LogoOnlyLayout';
+// guards
+import GuestGuard from '../guards/GuestGuard';
+import AuthGuard from '../guards/AuthGuard';
+import RoleBasedGuard from '../guards/RoleBasedGuard';
+// components
+import LoadingScreen from '../components/LoadingScreen';
+// paths
+import { PATH_ADMIN, PATH_LEARNING, PATH_PAGE } from './paths';
 
-
-
+// ----------------------------------------------------------------------
 
 const Loadable = (Component) => (props) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -29,10 +39,56 @@ export default function Router() {
         },
       ],
     },
+   
+    {
+      path: '/demo',
+      element: <Demo />,
+    },
+    {
+      path: '/loading',
+      element: <LoadingScreen fullScreen />,
+    },
+    {
+      path: '/',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to={PATH_LEARNING.test.root} replace />, index: true },
+        {
+          path: PATH_ADMIN.root,
+          element: (
+            <RoleBasedGuard accessibleRoles={['admin', 'mod']}>
+              <Admin />
+            </RoleBasedGuard>
+          ),
+        },
+        {
+     
+        {
+          path: '/profile',
+          element: (
+            <AuthGuard>
+              <Profile />
+            </AuthGuard>
+          ),
+        },
     
+          ),
+ 
     
+    {
+      path: '*',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: '404', element: <NotFound /> },
+        { path: '*', element: <Navigate to="/404" replace /> },
+      ],
+    },
+    { path: '*', element: <Navigate to="/404" replace /> },
   ]);
 }
 
 // Authentication
 const Login = Loadable(lazy(() => import('../pages/auth/Login')));
+
+
+const Profile = Loadable(lazy(() => import('../pages/Profile')));
